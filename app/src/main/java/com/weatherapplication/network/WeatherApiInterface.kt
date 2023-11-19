a933ce5a0b3f1e2be96a1e38af1b937e4502b7b5
stasek997 <robot1185@gmail.com>
2023-11-19 14:17:14 +0300

    Master

--- app/src/main/java/com/weatherapplication/network/WeatherApiInterface.kt
+++ app/src/main/java/com/weatherapplication/network/WeatherApiInterface.kt
@@ -1,5 +1,6 @@
 package com.weatherapplication.network
 
+import com.example.weatherapplication.BuildConfig
 import com.weatherapplication.data.WeatherModelDTO
 import retrofit2.Call
 import retrofit2.http.GET
@@ -12,7 +13,7 @@ interface WeatherApiInterface {
     @GET("/data/2.5/weather")
    suspend fun getWeather(
         @Query("id") cityId: Int,
-        @Query("appid") appId: String = "5404e1cc75e98a523480814e190b72fe"
+        @Query("appid") appId: String = BuildConfig.API_KEY
     ): WeatherModelDTO
 
 }
--- app/src/main/java/com/weatherapplication/repository/RequestCompleteListener.kt
+++
@@ -1,6 +0,0 @@
-package com.weatherapplication.repository
-
-//interface RequestCompleteListener<T> {
-  //  fun onRequestSuccess(data: T)
-    //fun onRequestFailed(errorMessage: String)
-//}
